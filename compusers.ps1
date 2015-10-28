1a616d85dd1cfc59efcb5ffd492b728e4397680f
oalba <odeialba@yahoo.es>
2015-10-28 11:59:55 +0000

    users array

--- compusers.ps1
+++ compusers.ps1
@@ -4,61 +4,22 @@ Import-Module activedirectory
 Workflow Test-Workflow {
 [CmdletBinding()]
         param(
-        [String]$user,
+        [String[]]$user,
         [String]$place
         )
 
-##########################################################################################
-####### CHANGE THIS TO DO FASTER #########################################################
-# uncomment needed lines and comment don't needed lines ##################################
-##########################################################################################
-                                                                                         #
-                                                                                         #
- #   $place = "DC=magni,DC=local"                                                        #
-                                                                                         #
-#    $place = "CN=Computers,DC=magni,DC=local"                                           #
-                                                                                         #
-#    $place = "OU=MAGNI Servers,DC=magni,DC=local"                                       #
-                                                                                         #
-#    $place = "OU=MAGNI Computers,DC=magni,DC=local"                                     #
-                                                                                         #
-#    $place = "OU=Ulster Folk and Transport Museum,OU=MAGNI Computers,DC=magni,DC=local" #
-                                                                                         #
-#    $place = "OU=Ulster Museum,OU=MAGNI Computers,DC=magni,DC=local"                    #
-                                                                                         #
-#    $place = "OU=Ulster American Folk Park,OU=MAGNI Computers,DC=magni,DC=local" 
-                                                                                         #
-##########################################################################################
-
-
-    Function Test-ConnectionQuietFast {
- 
-        [CmdletBinding()]
-        param(
-        [String]$ComputerName,
-        [int]$Count = 1,
-        [int]$Delay = 20
-        )
- 
-        for($I = 1; $I -lt $Count + 1 ; $i++)
-        {
-            If (Test-Connection -ComputerName $ComputerName -Quiet -Count 1)
-            {
-                return $True
-            }
-        }
-        $False
-    }
     $ErrorActionPreference= 'silentlycontinue'
 
     $Computers = Get-ADComputer -Filter * -SearchBase $place
-    "Username: $user"
+    Foreach –parallel -ThrottleLimit 100 ($us in $user) {
+        "Username: $us"
+    }
     "Place: $place"
 
     Foreach –parallel -ThrottleLimit 100 ($comp in $computers) {
         $Computer = $comp.Name
         $Computer = $Computer.Trim()
-        If (Test-ConnectionQuietFast -ComputerName $Computer -Count 1 -Delay 20) {
+        If (Test-Connection -ComputerName $Computer -Quiet -Count 1) {
             $sali = inlinescript{
                 if($Using:Computer){
                     $proc = Get-WmiObject win32_process -computer $Using:Computer -Filter "Name = 'explorer.exe'"
@@ -71,28 +32,32 @@ Workflow Test-Workflow {
                                 Username  = $Using:user
                                 IP  = $ips
                             }#>
-                        if($proc -eq $Using:user){
-                            #$sali | Format-Table ComputerName,LoggedUsers,Username,IP -AutoSize
-                            $sali = "$ips -------- $Using:Computer -------- $proc -------- $Using:user"; $sali
-                        } else {
-                            #$sali | Format-Table ComputerName,LoggedUsers -AutoSize
-                            $sali = "$Using:Computer  ----  $proc"; $sali
+                        foreach ($use in $Using:user){
+                            if($proc -eq $use){
+                                #$sali | Format-Table ComputerName,LoggedUsers,Username,IP -AutoSize
+                                $sali = "$ips -------- $Using:Computer -------- $proc -------- $use"; $sali
+                            }
                         }
+                        #$sali | Format-Table ComputerName,LoggedUsers -AutoSize
+                        $sali = "$Using:Computer  ----  $proc"; $sali
                     }
                 }
             }
             $sali
         }
     }
+    exit
 }
 
-$user = read-host “What is the Username of the user?”
-$UserCheck = get-aduser $user
+$user4 = read-host “What is the Username of the user?”
+$user2 = '*'+$user4+'*'
+$user3 = Get-ADUser -Filter {DisplayName -like $user2}| Select-Object samaccountname
+$user = $user3.samaccountname
 if ($user -eq ""){
-	Write-Host "Username cannot be blank, please re-enter username!!!!!"
-} elseif ($UserCheck -eq $null){
+	Write-Host "Username cannot be blank!!!!!"
+} elseif ($user -eq $null){
 	Write-Host $user
-	Write-Host "Invalid username, please verify this is the logon id for the account"
+	Write-Host "Invalid username, please verify this is the logon id for the account!!!!!"
 } else {
     $op = Read-Host "Where do you want to search?
     A) DC=magni,DC=local
